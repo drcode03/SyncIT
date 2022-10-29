45d87f973977046a2f2c7a7859b0afb39c0763b9
Jatin Grover <95739604+drcode03@users.noreply.github.com>
2022-10-30 02:46:54 +0530

    jk

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express')
 const fs = require("fs");
 const app = express()
 const server = require('http').Server(app)
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
+const io = require("socket.io")(server);
+
 
 const port = process.env.PORT || 30000;
 
