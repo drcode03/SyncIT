fb220b44619dfc5bcead19e5cbc67ec154a0d60e
drcode03 <ijatingrover@gmail.com>
2022-10-30 11:06:52 +0530

    dep test

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const fs = require("fs");
 const app = express()
-const server = require('http').Server(app)
+const server = require('http').createServer(app)
 const io = require('socket.io')(server);
 const port = process.env.PORT || 30000;
-
 app.set('view engine', 'ejs');
 const { ExpressPeerServer } = require('peer')
 const peerServer = ExpressPeerServer(server, {
@@ -17,14 +16,10 @@ app.use(express.static('pub'));
 //specifies the public files(accessible through browser by users) would live in the dir:pub !
 
 function create_UUID() {
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (dt + Math.random() * 16) % 16 | 0;
-    dt = Math.floor(dt / 16);
-    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-  });
-  return uuid;
-} //unique rand url generator
+
+    return Math.random()*5000;
+  };
+ //unique rand url generator
 let uuid = create_UUID();
 
 app.get('/', (req, res) => {
